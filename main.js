5e667679063e6f18607e8b6eb0a3c1c388825cac
Jon <jldeg@hotmail.com>
2023-01-03 22:24:45 +0100

    Added an illustration and changed the position of the buttons

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
-    let listOfPages = [
-        {
-          index: 0,
-          url: "https://www.codepen.io"
-        },
-        {
-          index: 1,
-          url: "https://www.css-tricks.com"
-        },
-        {
-          index: 2,
-          url: "https://blog.codepen.io/radio/"
-        },
-        {
-          index: 3,
-          url: "https://uxpodcast.com/"
+    let allButtons = document.querySelectorAll('a');
+    allButtons.forEach((button, index) => {
+        if(index === allButtons.length - 1) {
+            button.addEventListener('click', function(){
+                allButtons.forEach((button, index) => {
+                    if(index != 0) {
+                        button.classList.remove('clicked');
+                    }
+                });
+            })
+        } else {
+            button.addEventListener('click', function(){
+                button.classList.add('clicked');
+            })
         }
-      ];
-      
-      let iframeContainer = document.querySelector(".viewer");
-      
-      for (let i of listOfPages) {
-        iframeContainer.innerHTML += `<iframe src="${i.url}" frameborder="0" width="100%" height="100%"></iframe>`;
-      }
-    
+    });
 })
